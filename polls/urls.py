09117adb96581535c7560a864fe83beeb4a0511c
Helen-Teixeira-de-Souza <helensouza.gbi@gmail.com>
2026-05-15 15:36:36 -0300

    CRUD completo de pet

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,12 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path("", views.lista_pets, name="index"), 
+    path("", views.lista_pets, name="lista_pets"), 
     path('meus-pets/', views.lista_pets, name='pagina_inicial'),
     path('detalhes-pet/<int:pet_id>/', views.detalhes_pet, name='detalhes_pet'),
     path('vacinas/', views.historico_vacinas, name='pagina_vacinas'),
     path('consultas/', views.registro_consultas, name='pagina_consultas'),
+    path('novo/', views.novo_pet, name='novo_pet'),
+    path('edit/<int:pk>/', views.editar_pet, name='editar_pet'),
+    path('del/<int:pk>/', views.deletar_pet, name='deletar_pet'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Consulta, Pet, Vacina
+from .forms import PetForm
 
 def lista_pets(request):
     todos_os_pets = Pet.objects.all()
@@ -10,6 +11,26 @@ def detalhes_pet(request, pet_id):
     pet = Pet.objects.get(pk=pet_id)
     context = {'pet': pet}
     return render(request, 'polls/detalhes_pet.html', context)
+
+def novo_pet(request):
+    form = PetForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('polls:lista_pets')
+    return render(request, 'polls/pet_form.html', {'form': form})
+
+def editar_pet(request, pk):
+    pet = get_object_or_404(Pet, pk=pk)
+    form = PetForm(request.POST or None, instance=pet)
+    if form.is_valid():
+        form.save()
+        return redirect('polls:lista_pets')
+    return render(request, 'polls/pet_form.html', {'form': form})
+
+def deletar_pet(request, pk):
+    pet = get_object_or_404(Pet, pk=pk)
+    pet.delete()
+    return redirect('polls:lista_pets')
 
 def historico_vacinas(request):
     todas_as_vacinas = Vacina.objects.all().select_related('pet')
