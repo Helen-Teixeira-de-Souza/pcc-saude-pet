421c1dc113e95c2d2884efe398be44774dd34e5c
Helen-Teixeira-de-Souza <helensouza.gbi@gmail.com>
2026-05-27 14:55:41 -0300

    adicionar model tutor

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Pet(models.Model):
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, related_name='pets')
     nome = models.CharField(max_length=100)
     especie = models.CharField(max_length=50)
     raca = models.CharField(max_length=50, blank=True)
@@ -29,3 +30,19 @@ class Consulta(models.Model):
     
     def __str__(self):
         return f"Consulta {self.pet.nome} - {self.data_consulta}"
+
+class HistoricoPeso(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='historico_peso')
+    peso = models.FloatField(help_text="Peso em kg")
+    data_pesagem = models.DateField(auto_now_add=True) # para salvar a data automaticamente
+
+    def __str__(self):
+        return f"{self.pet.nome} - {self.peso}kg em {self.data_pesagem}"
+    
+class Tutor(models.Model):
+    nome = models.CharField(max_length=150)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
